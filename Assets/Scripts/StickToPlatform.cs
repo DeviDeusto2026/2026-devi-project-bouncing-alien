d5ab3ba4aef1a3930f7f3e9e28f73be51300e081
Lorena732 <lorena.s@opendeusto.es>
2026-05-22 09:10:59 +0200

    Moving platform

--- Assets/Scripts/StickToPlatform.cs
+++ Assets/Scripts/StickToPlatform.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Platform : MonoBehaviour
+public class StickToPlatform : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
