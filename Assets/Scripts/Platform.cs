2bcfc6f50c3037b8145a6a7597f17c88f987ad5b
AsierBascones <asier.bascones@opendeusto.es>
2026-05-22 09:57:08 +0200

    Platforms fix

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    // Este script identifica que el objeto es una Zona de Gravedad Local.
+    // Usaremos su transform.up para saber dónde está "arriba".
+}
